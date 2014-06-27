f53aad005f8f23ea86b1885e845b87f4ff1b90ab
Patrick Jones <pmichaeljones@gmail.com>
2014-06-26 18:07:49 -0600

    added more views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,36 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
 
 end
