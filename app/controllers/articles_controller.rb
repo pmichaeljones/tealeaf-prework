1e2afa469fe71b9cbd8e0eaa622f9cf0057c1b54
Patrick Jones <pmichaeljones@gmail.com>
2014-06-27 09:12:52 -0600

    added new form render

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,18 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
+
   def edit
     @article = Article.find(params[:id])
   end
